749ec98ed93e5b57a1518895bd18aa1b1cdc685a
Harsh <harshrawat096@gmail.com>
2022-12-17 09:38:23 +0530

    c

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -137,7 +137,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 import os
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   
+]
+
